420be9c62c005231eb45ca3c731ab7a5ad6883a1
RAS <user@MacBook.local>
2024-10-02 12:57:02 +0700

    add: Image, padding, marghin

--- lib/main.dart
+++ lib/main.dart
@@ -18,17 +18,34 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Flutter Learning"),
-            backgroundColor: Colors.blue,
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {},
-              ),
+        appBar: AppBar(
+          title: const Text("Flutter Learning"),
+          backgroundColor: Colors.blue,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: SafeArea(
+            child: Container(
+          margin:
+              const EdgeInsets.only(left: 50.0, top: 0, bottom: 0, right: 50.0),
+          child: const Column(
+            children: <Widget>[
+              Image(
+                  image: AssetImage(
+                    "assets/images/ic_payment.jpg",
+                  ),
+                  height: 300,
+                  width: 300),
+              Text("Rich Together"),
+              Text("Save your money little bit and rich together"),
             ],
           ),
-          body: SafeArea(child: Text("Hello World"))),
+        )),
+      ),
     );
   }
 }
